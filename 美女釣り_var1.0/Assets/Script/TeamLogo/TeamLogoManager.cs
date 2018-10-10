ce54bfa9cd878876425682ac089c56120d176d74
geskysan <sakuya3983@ezweb.ne.jp>
2018-10-10 12:10:53 +0900

    フェード追加完了 チームロゴ完成

---
+++ 美女釣り_var1.0/Assets/Script/TeamLogo/TeamLogoManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TeamLogoManager : MonoBehaviour {
+
+    [SerializeField]
+    CanvasGroup m_TeamLogo;
+    [SerializeField]
+    float m_fadeTime;
+
+    // Use this for initialization
+    void Start() {
+        Sequence seq = DOTween.Sequence();
+
+        seq.OnStart(() =>
+        {
+            m_TeamLogo.alpha = 0f;
+        })
+        .Append(m_TeamLogo.DOFade(1f, m_fadeTime))
+        .AppendInterval(m_fadeTime / 2f)
+        .Append(m_TeamLogo.DOFade(0f, m_fadeTime))
+        .OnComplete(() =>
+        {
+            SceneChange();
+        });
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SceneChange()
+    {
+        SceneNavigator.Instance.Change("title");
+    }
+}
