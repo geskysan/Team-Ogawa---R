731f418f918a88038c70972ee8c33f96b587db49
paku <naoya19971008@gmail.com>
2018-10-17 11:22:17 +0900

    tyomuraisu

---
+++ 美女釣り_var1.0/Assets/Script/GameFinish.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameFinish : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GoFishing()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void GoLure()
+    {
+        SceneManager.LoadScene("menu-lure");
+    }
+
+    public void GoGacha()
+    {
+        SceneManager.LoadScene("menu-gacha");
+    }
+}
