80edbf3577c3f7605271c417f8189696acfd73c9
geskysan <sakuya3983@ezweb.ne.jp>
2018-10-18 10:10:28 +0900

    タップした位置にルアーが移動するようになっています

--- 美女釣り_var1.0/Assets/Script/Main/LureController.cs
+++ 美女釣り_var1.0/Assets/Script/Main/LureController.cs
@@ -5,65 +5,32 @@ using UnityEngine;
 public class LureController : MonoBehaviour {
 
     [SerializeField]
-    GameObject m_lureObj;
+    private GameObject m_lureObj;
 
     // タップ時の開始座標
-    private Vector2 m_startTapPos;
-    // 指を離した時の座標
-    private Vector2 m_endTapPos;
+    private Vector2 m_tapPos;
 
     // Use this for initialization
     void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        Tap();
     }
 
-    void Flick()
+
+    // タップした位置に移動
+    void Tap()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if(Input.GetMouseButtonDown(0))
         {
-            m_startTapPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            // タップした位置
+            m_tapPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            m_endTapPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
-        }
-
-        GetDirection();
-    }
-
-    void GetDirection()
-    {
-        float directionX = m_endTapPos.x - m_startTapPos.x;
-        float directionY = m_endTapPos.y - m_startTapPos.y;
-
-        if(Mathf.Abs(directionY) < Mathf.Abs(directionX))
-        {
-            if(30 < directionX)
-            {
-                // 右にフリック
-            }
-            else if(-30 > directionX)
-            {
-                // 左にフリック
-            }
-        }
-        else if(Mathf.Abs(directionX) <Mathf.Abs(directionY))
-        {
-            if(30 < directionY)
-            {
-                // 上にフリック
-            }
-            else if(-30 >directionY)
-            {
-                // 下にフリック
-            }
-
-        }
+        // Lureの位置をクリック地点に移動
+        m_lureObj.transform.localPosition = new Vector3(m_tapPos.x - (Screen.width / 2), m_tapPos.y - (Screen.height / 2), 0f);
     }
 }
