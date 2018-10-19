8c285c5c7f51471a80dd4b4885125427bb2a4112
geskysan <sakuya3983@ezweb.ne.jp>
2018-10-19 15:53:43 +0900

    美女を釣ろうと思いましたが、寝すぎてまだできていません

---
+++ 美女釣り_var1.0/Assets/Script/Main/GirlsMoveController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlsMoveController : MonoBehaviour {
+
+    [SerializeField] float m_speed;
+
+    private void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    /// <summary>
+    /// 美女が画面内を縦横無尽に動き回る
+    /// </summary>
+    /// <param name="direction">方向転換の方向</param>
+    void BijoMove(float direction)
+    {
+
+    }
+
+    public void OnCollitionHit()
+    {
+        var random = 0;
+
+        // ランダムな値の角度を入れる
+        random = Random.Range(0, 360);
+
+        // 取得した角度に方向転換
+        BijoMove(random);
+    }
+}
