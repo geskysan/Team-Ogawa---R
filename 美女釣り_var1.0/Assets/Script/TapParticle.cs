b592d205b7dc1921a5da6247eb1d72879883432c
paku <naoya19971008@gmail.com>
2018-10-24 14:36:15 +0900

    menu end

---
+++ 美女釣り_var1.0/Assets/Script/TapParticle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapParticle : MonoBehaviour {
+
+    [SerializeField]
+    private ParticleSystem particle;
+
+    [SerializeField]
+    private Camera _camera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // マウスのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
+            var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
+            particle.transform.position = pos;
+            particle.Emit(1);
+        }
+    }
+}
