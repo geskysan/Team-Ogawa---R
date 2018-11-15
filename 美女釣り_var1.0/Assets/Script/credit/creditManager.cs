67f9ec753aa755340ee883a1fe59187b2404bb61
omnk1919810 <aiueolalesnow1@gmail.com>
2018-11-15 09:57:46 +0900

    クレジットやった
    クレジット完成

---
+++ 美女釣り_var1.0/Assets/Script/credit/creditManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class creditManager : MonoBehaviour {
+
+    [SerializeField] CanvasGroup Logo;
+    [SerializeField] GameObject creditText;
+
+	// Use this for initialization
+	void Start () {
+        Sequence seq = DOTween.Sequence();
+        seq.OnStart(() =>
+        {
+            Logo.alpha = 0f;
+        })
+            .AppendInterval(1f)
+            .Append(Logo.DOFade(1f, 1f))
+            .AppendInterval(2f)
+            .Append(Logo.DOFade(0f, 1f))
+            .AppendInterval(1f)
+            .Append(
+            creditText.gameObject.transform.DOLocalMoveY(4600.0f, 40f).SetEase(Ease.Linear))
+            .Append(Logo.DOFade(1f, 1f))
+            .AppendInterval(2f)
+            .Append(Logo.DOFade(0f, 1f))
+            .AppendInterval(1f)
+            .OnComplete(() =>
+            {
+                //シーン移動
+            });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
