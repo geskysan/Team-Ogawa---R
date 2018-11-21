e5a69eac98b74ea3560d1fdb2cc0de9d60401f01
geskysan <sakuya3983@ezweb.ne.jp>
2018-11-21 09:14:29 +0900

    全てのシーン追加 α版完成

---
+++ BIJO/Assets/Script/TapParticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapParticle : MonoBehaviour {
+
+    [SerializeField,TooltipAttribute("Particle")]
+    private ParticleSystem particle;
+
+    [SerializeField, TooltipAttribute("Camera")]
+    private Camera _camera;
+
+    void Start () {
+
+    }
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
+            particle.transform.position = pos;
+            particle.Emit(1);
+        }
+    }
+}
