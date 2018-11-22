3f79cc4163f009a77bfe278185801a6df6b3ce27
omnk1919810 <aiueolalesnow1@gmail.com>
2018-11-22 09:55:02 +0900

    androidできへん
    マ？

---
+++ 美女釣り_var1.0/美女釣り_var1.0/Assets/Script/TeamLogo/TeamLogoManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TeamLogoManager : MonoBehaviour {
+
+    [SerializeField]
+    CanvasGroup m_TeamLogo;
+    [SerializeField]
+    float m_fadeTime;
+
+    // Use this for initialization
+    void Start() {
+
+        SoundScript.Instance.PlaySE(SoundNameData.SE_OGAWA, 0.7f);
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.OnStart(() =>
+        {
+            m_TeamLogo.alpha = 0f;
+        })
+        .AppendInterval(m_fadeTime / 2f)
+        .Append(m_TeamLogo.DOFade(1f, m_fadeTime))
+        .AppendInterval(m_fadeTime / 2f)
+        .Append(m_TeamLogo.DOFade(0f, m_fadeTime))
+        .OnComplete(() =>
+        {
+            SceneChange();
+        });
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SceneChange()
+    {
+        SceneNavigator.Instance.Change("title");
+    }
+}
---
+++ 美女釣り_var1.0/美女釣り_var1.0/Assets/Script/Title/TitleControler.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class TitleControler : MonoBehaviour {
+
+    [SerializeField]
+    CanvasGroup m_tapLogo; //ロゴ
+    [SerializeField]
+    CanvasGroup m_titleLogo; //タイトルロゴ
+    [SerializeField]
+    GameObject NewPlayer; //そんまんま
+
+    [SerializeField] float m_scaleSize = 1.2f;
+    [SerializeField] float m_scaleTime;
+    [SerializeField] float m_titleFadeTime;
+
+    [SerializeField] private float DurationSeconds;
+    [SerializeField] private Ease EaseType;
+
+    [SerializeField]
+    InputField inputField;
+    [SerializeField]
+    Text Nameinput;
+    [SerializeField]
+    GameObject tapsiteyo; //tap
+    [SerializeField]
+    Text ikuyo;
+
+    string SERVER = "153.126.208.136"; //さくらVPSに接続してる
+    string DATABASE = "bjo";
+    string USERID = "testplayer";
+    string PORT = "3306";
+    string PASSWORD = "Testplayer_1";
+
+    string username;
+    object userid;
+   // string TABLENAME = "user";
+
+    // Use this for initialization
+    void Start()
+    {
+        //タイトルの動き
+        Sequence seq = DOTween.Sequence()
+            .OnStart(() =>
+            {
+                m_titleLogo.GetComponent<GameObject>();
+                m_titleLogo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            })
+        .Append(m_titleLogo.transform.DOScale(1.0f, m_scaleTime))
+        .Join(m_titleLogo.DOFade(1.0f, m_titleFadeTime))
+        .Append(m_titleLogo.transform.DOScale(0.9f, 0.5f))
+        .Join(m_titleLogo.transform.DORotate(new Vector3(0.0f, 0.0f, -10.0f), 1.5f))
+        .OnComplete(() =>
+        {
+        m_titleLogo.transform.DORotate(new Vector3(0.0f, 0.0f, 10.0f), 1.5f).SetLoops(-1, LoopType.Yoyo);
+
+        });
+
+        SoundScript.Instance.PlayBGM(SoundNameData.BGM_TITLE);
+        m_tapLogo.DOFade(0.5f, this.DurationSeconds).SetEase(this.EaseType).SetLoops(-1, LoopType.Yoyo);
+
+        string connCmd =
+                  "server=" + SERVER + ";" +
+                  "database=" + DATABASE + ";" +
+                  "userid=" + USERID + ";" +
+                  "port=" + PORT + ";" +
+                  "password=" + PASSWORD;
+     
+        MySqlConnection conn = new MySqlConnection(connCmd);
+        //ここUSERIDを取ってる
+        try
+        {
+           
+            Debug.Log("MySQLと接続中...");
+            conn.Open();
+            ikuyo.gameObject.SetActive(true);
+            string sql = "SELECT MAX( userid ) FROM user;";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                userid = rdr[0];
+                Debug.Log(rdr[0]);
+            
+            }
+            rdr.Close();
+
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        conn.Close();
+        Debug.Log("接続を終了しました");
+
+    }
+
+
+
+// Update is called once per frame
+void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (!PlayerPrefs.HasKey("Init"))
+            {
+                NewPlayer.gameObject.SetActive(true);
+            }
+            if (PlayerPrefs.HasKey("Init"))
+            {
+                SceneNavigator.Instance.Change("menu");
+            }
+        }
+    }
+
+    //ユーザー作るときの奴
+    public void Decision()
+    {
+        //空白やNULLじゃなかったら登録する
+        if (!string.IsNullOrEmpty(username))
+        {
+            int userid_mysql = (int)userid;
+            Debug.Log(userid_mysql);
+            string connCmd =
+                    "server=" + SERVER + ";" +
+                    "database=" + DATABASE + ";" +
+                    "userid=" + USERID + ";" +
+                    "port=" + PORT + ";" +
+                    "password=" + PASSWORD;
+
+            MySqlConnection conn = new MySqlConnection(connCmd);
+
+            try
+            {
+                Debug.Log("MySQLと接続中...");
+                conn.Open();
+
+                string sql = "INSERT INTO user (userid,username,highscore) values (" + userid_mysql + "+1," + "\'" + username + "\'" + ",0);";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                Debug.Log(sql);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.ToString());
+            }
+            conn.Close();
+
+            PlayerPrefs.SetInt("Init", 1); // ”Init”のキーをint型の値(1)で保存
+            PlayerPrefs.SetInt("user_id", userid_mysql + 1); //userid保存
+
+            Debug.Log("接続を終了しました");
+
+            NewPlayer.gameObject.SetActive(false);
+            tapsiteyo.gameObject.SetActive(true);
+        }
+        else
+        {
+            //空白やNULLだったら赤文字にする
+            Nameinput.color = new Color (255f, 0f, 0f);
+        }
+    }
+    //名前入力
+    public void FieldInput()
+    {
+        username = inputField.text;
+        //空白は許さん
+        if(inputField.text == " ")
+        {
+            inputField.text = "";
+        }
+        Debug.Log(username);
+    }
+}
+
