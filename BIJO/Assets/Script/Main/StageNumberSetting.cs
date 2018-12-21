a51974742cc80ad7afcf7214384b3314c7eb03f6
geskysan <sakuya3983@ezweb.ne.jp>
2018-12-21 09:11:49 +0900

    1919

--- BIJO/Assets/Script/Main/StageNumberSetting.cs
+++ BIJO/Assets/Script/Main/StageNumberSetting.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 public class StageNumberSetting : MonoBehaviour {
 
+    [SerializeField] StartUpManager m_startUpmanager;
+
     public enum STAGE
     {
         stage_1,
@@ -14,6 +16,8 @@ public class StageNumberSetting : MonoBehaviour {
         stage_6,
         stage_7,
     }
+    [SerializeField] string[] m_stageName;
+    [SerializeField] Text m_staeNameText;
 
     enum SELECTTYPE
     {
@@ -36,7 +40,12 @@ public class StageNumberSetting : MonoBehaviour {
         {
             if (m_stage == (STAGE)i)
             {
+                // ステージの背景を設定
                 m_background.GetComponent<Image>().sprite = m_stageImage[i];
+                m_startUpmanager.m_background = m_stageImage[i];
+
+                // ステージ名を設定
+
 
                 if(particle[i] != null)
                 {
@@ -47,39 +56,5 @@ public class StageNumberSetting : MonoBehaviour {
                 break;
             }
         }
-
-        //switch (m_SelectType)
-        //{
-        //    case SELECTTYPE.Order:
-        //        for (int i = 0; i < m_stageImage.Length; i++)
-        //        {
-        //            if (m_stage == (STAGE)i)
-        //            {
-        //                m_background.GetComponent<Image>().sprite = m_stageImage[i];
-        //                Instantiate(particle[i]);
-        //                particle[i].Play();
-        //                break;
-        //            }
-        //        }
-
-        //    case SELECTTYPE.Random:
-        //        var random = 0;
-        //        random = Random.Range(0, m_stageImage.Length);
-        //        for (int i = 0; i < m_stageImage.Length; i++)
-        //        {
-        //            if (random == i)
-        //            {
-        //                m_background.GetComponent<Image>().sprite = m_stageImage[i];
-        //                Instantiate(particle[i]);
-        //                particle[i].Play();
-        //                break;
-        //            }
-        //        }
-
-        //    default:
-        //        break;
-        //}
-
-
     }
 }
--- BIJO/Assets/Script/Main/StartUpManager.cs
+++ BIJO/Assets/Script/Main/StartUpManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class StartUpManager : MonoBehaviour {
@@ -10,20 +11,49 @@ public class StartUpManager : MonoBehaviour {
     [SerializeField] CanvasGroup m_ready, m_go;
     [SerializeField] float m_readyTime = 3f;
 
+    public Text m_stageNameText;
+    public Sprite m_background; 
+
     public bool m_OK = false;
 
 	// Use this for initialization
 	void Start () {
+
+	}
+
+    void StageName()
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.OnStart(() =>
+        {
+
+        })
+
+        .OnComplete(() =>
+        {
+
+        });
+    }
+
+    void StartUp()
+    {
         Sequence seq = DOTween.Sequence();
         seq.OnStart(() =>
         {
+            // Ready,Goを透明にする
             m_ready.alpha = 0f;
             m_go.alpha = 0f;
         })
+        // readyをフェードの出現
         .Append(m_ready.DOFade(1f, 1f))
+        // 待機
         .AppendInterval(m_readyTime)
+        // readyを非表示
         .Append(m_ready.DOFade(0f, 0.1f))
+        // readyが消えるのと同時にGO表示
         .Join(m_go.DOFade(1f, 0.1f))
+        // GOを少しずつ大きく
         .Join(m_go.transform.DOScale(1.5f, 1f))
         .AppendInterval(0.5f)
         .OnComplete(() =>
@@ -32,7 +62,5 @@ public class StartUpManager : MonoBehaviour {
             this.gameObject.SetActive(false);
             m_girlsManager.GirlsStartUp();
         });
-	}
-
-
+    }
 }
